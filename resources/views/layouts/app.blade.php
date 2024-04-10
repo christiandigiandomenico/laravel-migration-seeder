c9c2ee5e65403bdfc6ab101997d580b3c316e31b
christiandigiandomenico <christian.digiandomenico1990@gmail.com>
2024-04-10 17:11:47 +0200

    Esercizio terminato

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Titolo</title>
+    <title>Train Database</title>
     @vite('resources/js/app.js')
 </head>
 <body>
+
+    <h1>Tabella dei Treni</h1>
+
+    <div class="container">
     
+    @foreach($trains as $train)
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">{{$train->company}}</h5>
+          <h6 class="card-subtitle mb-2 text-body-secondary">Stazione di partenza: {{$train->departure_station}}</h6>
+          <h6 class="card-subtitle mb-2 text-body-secondary">Stazione di arrivo: {{$train->arrival_station}}</h6>
+          <p class="card-text">Orario di partenza: {{$train->departure_time}}</p>
+          <p class="card-text">Orario di arrivo: {{$train->arrival_time}}</p>
+          <p class="card-text">Codice treno: {{$train->train_code}}</p>
+          <p class="card-text">Numero carrozze: {{$train->train_carriage}}</p>
+          <p class="card-text">In orario: {{$train->on_schedule ? 'Sì' : 'No'}}</p>
+          <p class="card-text">Cancellato: {{$train->cancelled ? 'Sì' : 'No'}}</p>
+        </div>
+      </div>
+    @endforeach
+
+    </div>
 
 </body>
 </html>
